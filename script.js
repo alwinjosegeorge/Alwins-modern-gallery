8a58b2fb55b85e6cec09aec72175c90b50f2d34d
Alwin Jose George <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2025-07-13 18:00:19 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// This code creates the fade-in-on-scroll effect
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+    });
+}, {
+    threshold: 0.1 // Trigger when 10% of the element is visible
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
